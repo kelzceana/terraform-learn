93d51559f42a4686deb6a3f9b209e59dcbaaac5d
Kelechi Obikaonu <kelechiobikaonu@Kelechis-MacBook-Air.local>
2024-11-21 15:27:24 -0500

    created the vpc, subnet and Sg for the ec2 instance

--- terraform-ec2/main.tf
+++ terraform-ec2/main.tf
@@ -7,6 +7,7 @@ variable vpc_cidr_blocks {}
 variable subnet_cidr_blocks {}
 variable avail_zone {}
 variable env_prefix {}
+variable my_ip{}
 
 #resources
 resource "aws_vpc" "myapp-vpc" {
@@ -49,8 +50,8 @@ resource "aws_internet_gateway" "myapp-igw" {
     }
 }
 
-resource "aws_default_route_table" "main-route-table" {
-    default_route_table.id = aws_vpc.myapp-vpc.default_route_table_id
+resource "aws_default_route_table" "main-rtb" {
+    default_route_table_id = aws_vpc.myapp-vpc.default_route_table_id
     route {
          cidr_block = "0.0.0.0/0"
          gateway_id = aws_internet_gateway.myapp-igw.id
@@ -58,4 +59,33 @@ resource "aws_default_route_table" "main-route-table" {
      tags = {
          Name: "${var.env_prefix}-main_rtb"
      }
+}
+
+resource "aws_default_security_group" "default-sg" {
+    vpc_id = aws_vpc.myapp-vpc.id
+
+    ingress {
+        from_port = 22
+        to_port = 22
+        protocol = "TCP"
+        cidr_blocks = [var.my_ip]
+    }
+
+    ingress {
+        from_port = 8080
+        to_port = 8080
+        protocol = "TCP"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    tags = {
+         Name: "${var.env_prefix}-default-sg"
+     }
+
 }
