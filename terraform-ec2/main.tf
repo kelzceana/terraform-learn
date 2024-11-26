a529652ccf0ec1fe010198afaa8a38faec2ca3c1
Kelechi Obikaonu <kelechiobikaonu@Kelechis-MacBook-Air.local>
2024-11-25 23:13:14 -0500

    updated the ec2 resource

--- terraform-ec2/main.tf
+++ terraform-ec2/main.tf
@@ -9,6 +9,7 @@ variable avail_zone {}
 variable env_prefix {}
 variable my_ip {}
 variable instance_type {}
+variable public_key_location {}
 
 #resources
 resource "aws_vpc" "myapp-vpc" {
@@ -104,11 +105,19 @@ data "aws_ami" "latest-aws-linux-image" {
     }
 }
 
-# output "aws_ami_id" {
-#     value = data.aws_ami.latest-aws-linux-image.id
-# }
+output "aws_ami_id" {
+    value = data.aws_ami.latest-aws-linux-image.id
+}
+output "ec2_public_ip" {
+    value = aws_instance.myapp_server.public_ip
+}
 
-resource "aws_instance" "myapp_serer" {
+resource "aws_key_pair" "ssh-key" {
+    key_name = "server-key"
+    public_key = file(var.public_key_location)
+}
+
+resource "aws_instance" "myapp_server" {
     ami = data.aws_ami.latest-aws-linux-image.id
     instance_type = var.instance_type
 
@@ -116,7 +125,7 @@ resource "aws_instance" "myapp_serer" {
     vpc_security_group_ids = [aws_default_security_group.default-sg.id]
     availability_zone = var.avail_zone
     associate_public_ip_address = true
-    key_name = "server-key-pair"
+    key_name = aws_key_pair.ssh-key.key_name
 
     tags = {
          Name: "${var.env_prefix}-server"
